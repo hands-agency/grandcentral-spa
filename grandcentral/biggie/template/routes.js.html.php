870c90902a308b7deba8c1d60679a26870ade5f6
victor-gabou <victor.gabou@outlook.fr>
2017-08-04 13:25:19 +0200

    use favorite navigator languages to set default lang

--- grandcentral/biggie/template/routes.js.html.php
+++ grandcentral/biggie/template/routes.js.html.php
@@ -6,6 +6,12 @@
 	$assets = $_PARAM;
 	array_shift($assets);
 
+	$langs = [];
+	foreach ($versions as $version)
+	{
+		$langs[] = $version['lang'];
+	}
+
 ?>
 import config from 'config'
 
@@ -14,22 +20,30 @@ all routes needs to be defined inline
 see https://github.com/bigwheel-framework/documentation/blob/master/routes-defining.md#as-section-standard-form
 ---------- */
 
+var allowedLanguages = ['<?= implode('\',\'', $langs) ?>']
+var favoriteLanguages = navigator.languages
+var defaultLang = false
+for (var i = 0; i < favoriteLanguages.length; i++) {
+	if (!defaultLang && allowedLanguages.indexOf(favoriteLanguages[i]) != -1) defaultLang = favoriteLanguages[i]
+}
+if (!defaultLang) defaultLang = allowedLanguages[0]
+
 module.exports = {
 <?php foreach ($versions as $version): ?>
 	// <?= $version['title'] ?>
 
 <?php foreach ($assets as $asset):
-	$url = $version['key'].$asset['url'][$version['key']->get()];
+	$url = $asset['url'][$version['key']->get()];
 	if (isset($asset['type']) && $asset['type'] == 'reader_detail') $url .= '/:id';
 	if ($asset['key'] == 'home'):
 		$homeUrl = empty($homeUrl) ? $url : $homeUrl;
 ?>
 	[`${config.BASE}<?= $version['key'] ?>`]: '/<?= $url ?>',
 <?php endif; ?>
-	[`${config.BASE}<?= $url ?>`]: require('<?= mb_substr($asset['template']['js'], 0, -3) ?>'),
+	[`${config.BASE}<?= $version['key'].$url ?>`]: require('<?= mb_substr($asset['template']['js'], 0, -3) ?>'),
 <?php endforeach; ?>
 <?php endforeach; ?>
 	// other routes
-	[`${config.BASE}`]: '/<?= $versions[0]['key'] ?>',
-	'404': '/<?= $homeUrl ?>'
+	[`${config.BASE}`]: `/${defaultLang}<?= $homeUrl ?>`,
+	'404': `/${defaultLang}`
 }
