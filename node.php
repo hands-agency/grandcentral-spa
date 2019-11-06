f7482d0bdcda40db6eed2fbb3e010eec110624af
Sylvain Frigui <sf@hands.agency>
2019-11-06 16:23:42 +0100

    feat(node): dirty export mechanic

--- node.php
+++ node.php
@@ -32,4 +32,58 @@
       break;
 
   }
+
+  // $tables = ['board','const','contact','event','initiative','insight','news','page','press','video'];
+  // $attrs = ['title','shortdescr','descr','location','address','timezone','content','sourcetitle','type','information','displaytitle'];
+  // $params = ['title','descr','text','video1_title','video2_title','video3_title','heading','teamtitle','teamheading','teamdescr','foundertitle','founderheading','founderdescr','boardtitle','boardheading','boarddescr','content','catchphrase','button','intro','start','outro'];
+  // $i = 0;
+  // $lang = 'en';
+  // echo '<table cellpadding="0" cellspacing="0" border="1">';
+  // foreach ($tables as $table)
+  // {
+  //   $datas = i($table,all);
+  //
+  //   foreach ($datas as $data)
+  //   {
+  //     foreach ($data as $key => $attr)
+  //     {
+  //       if (in_array($key, $attrs))
+  //       {
+  //         if ($key != 'type')
+  //         {
+  //           echo '<tr>
+  //             <td>'.$data->get_nickname().'</td>
+  //             <td>'.$key.'</td>
+  //             <td></td>
+  //             <td>'.$attr.'</td>
+  //           </tr>';
+  //         }
+  //         elseif (isset($attr['master']['param']))
+  //         {
+  //           foreach ($attr['master']['param'] as $pkey => $pvalue)
+  //           {
+  //             if (in_array($pkey, $params))
+  //             {
+  //               if (mb_strstr($pvalue[$lang],'{"data"')) {
+  //                 $a = new attrSirtrevor($pvalue[$lang]);
+  //                 $v = (string) $a;
+  //               }
+  //               else
+  //               {
+  //                 $v = $pvalue[$lang];
+  //               }
+  //               echo '<tr>
+  //                 <td>'.$data->get_nickname().'</td>
+  //                 <td>'.$key.'</td>
+  //                 <td>'.$pkey.'</td>
+  //                 <td>'.$v.'</td>
+  //               </tr>';
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+  // echo '</table>';
 ?>
